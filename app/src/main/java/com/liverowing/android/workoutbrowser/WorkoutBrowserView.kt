4cb0d159adc98f11b21e7e541de664f37b16c825
CUREST TREBOW <curest.trebow@gmail.com>
2018-08-20 16:57:30 +0200

    WBV

--- app/src/main/java/com/liverowing/android/workoutbrowser/WorkoutBrowserView.kt
+++
@@ -1,6 +0,0 @@
-package com.liverowing.android.workoutbrowser
-
-import com.hannesdorfmann.mosby3.mvp.lce.MvpLceView
-import com.liverowing.android.model.parse.WorkoutType
-
-interface WorkoutBrowserView : MvpLceView<List<WorkoutType>>
