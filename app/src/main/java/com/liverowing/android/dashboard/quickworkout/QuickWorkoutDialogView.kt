08c85d9ea7240fc8b31252f991efbf5e906f7c82
CUREST TREBOW <curest.trebow@gmail.com>
2018-08-20 16:58:25 +0200

    QWDV

---
+++ app/src/main/java/com/liverowing/android/dashboard/quickworkout/QuickWorkoutDialogView.kt
@@ -0,0 +1,5 @@
+package com.liverowing.android.dashboard.quickworkout
+
+import com.hannesdorfmann.mosby3.mvp.lce.MvpLceView
+
+interface QuickWorkoutDialogView : MvpLceView<List<QuickWorkoutDialogPresenter.QuickWorkoutItem>>
