10b8ff5ec5faa0f30480ba67aa07344908275434
CUREST TREBOW <curest.trebow@gmail.com>
2018-08-24 20:33:26 +0800

    SummaryVC layout

--- app/src/main/java/com/liverowing/android/workouthistory/detail/summary/WorkoutHistorySummaryFragment.kt
+++ app/src/main/java/com/liverowing/android/workouthistory/detail/summary/WorkoutHistorySummaryFragment.kt
@@ -6,13 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.liverowing.android.LiveRowing
 import com.liverowing.android.R
+import org.greenrobot.eventbus.EventBus
 
 class WorkoutHistorySummaryFragment : Fragment() {
+
+    override fun onStart() {
+        super.onStart()
+        EventBus.getDefault().register(this@WorkoutHistorySummaryFragment)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        EventBus.getDefault().unregister(this@WorkoutHistorySummaryFragment)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        LiveRowing.refWatcher(this.activity).watch(this)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_workout_history_detail_summary, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 
