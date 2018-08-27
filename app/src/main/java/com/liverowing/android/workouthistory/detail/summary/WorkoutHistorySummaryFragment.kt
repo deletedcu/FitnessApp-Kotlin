ea5478d243e73e38ab99872d22822c91dc4488dd
CUREST TREBOW <curest.trebow@gmail.com>
2018-08-27 09:44:55 +0800

    Implemented the WorkoutSummaryVC - Split dialog

--- app/src/main/java/com/liverowing/android/workouthistory/detail/summary/WorkoutHistorySummaryFragment.kt
+++ app/src/main/java/com/liverowing/android/workouthistory/detail/summary/WorkoutHistorySummaryFragment.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.request.RequestOptions
 import com.liverowing.android.LiveRowing
 import com.liverowing.android.R
 import com.liverowing.android.model.parse.Workout
-import com.liverowing.android.model.parse.WorkoutType
+import com.liverowing.android.model.pm.SplitTitle
+import com.liverowing.android.model.pm.SplitType
 import kotlinx.android.synthetic.main.fragment_workout_history_detail_summary.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
@@ -20,6 +24,12 @@ import org.greenrobot.eventbus.ThreadMode
 import java.text.SimpleDateFormat
 
 class WorkoutHistorySummaryFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private var titles = SplitTitle.defaultData()
+    private var dataSet = mutableListOf<MutableMap<SplitType, Number>>()
 
     override fun onStart() {
         super.onStart()
@@ -44,6 +54,7 @@ class WorkoutHistorySummaryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupUI()
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
@@ -64,5 +75,34 @@ class WorkoutHistorySummaryFragment : Fragment() {
         f_workout_history_detail_name.text = workoutType?.createdBy?.userClass
         f_workout_history_detail_createdat.text = SimpleDateFormat.getDateInstance().format(workout.createdAt)
 
+        dataSet.clear()
+        if (workout.data.WorkoutData.splits is List<Workout.Split>) {
+            val splits = workout.data.WorkoutData.splits
+            splits.forEach {
+                dataSet.add(it.getMap())
+            }
+            viewAdapter.notifyDataSetChanged()
+        }
     }
+
+    private fun setupUI() {
+        viewManager = LinearLayoutManager(activity!!)
+        viewAdapter = WorkoutHistorySummarySplitsAdapter(dataSet, titles)
+
+        recyclerView = f_workout_history_detail_summary_splits_recyclerview.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+        f_workout_history_detail_summary_more.setOnClickListener {
+            showSplitDialog()
+        }
+    }
+
+    private fun showSplitDialog() {
+        val dialogFragment = SplitDialogFragment(titles)
+        dialogFragment.show(fragmentManager, dialogFragment.javaClass.toString())
+    }
+
 }
