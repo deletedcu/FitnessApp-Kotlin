dee21b1649258926cb2558f10c60a2440ee22f1c
CUREST TREBOW <curest.trebow@gmail.com>
2018-09-01 01:40:03 +0800

    Filter Dialog surface has incorrect radius on corners.

--- app/src/main/java/com/liverowing/android/dashboard/FeaturedChooseDialogFragment.kt
+++ app/src/main/java/com/liverowing/android/dashboard/FeaturedChooseDialogFragment.kt
@@ -25,7 +25,6 @@ class FeaturedChooseDialogFragment(private var featuredList: MutableList<User>, 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.dialog_choose_featured, container, false)
-        dialog.window.setBackgroundDrawableResource(R.drawable.dialog_background)
         setupUI(view)
         return view
     }
